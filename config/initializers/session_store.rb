724295fbdaace73ba821842eaa7c4573314ce8cf
wl88 <rf810808@gmail.com>
2014-07-07 20:51:08 -0400

    test for deploy

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,18 @@
+# Be sure to restart your server when you modify this file.
+
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+Project1::Application.config.secret_key_base = secure_token
